2629bcab1543af74afbf185abcd20414bb437f4a
Claudio Bosticco <c.bosticco@sistemitre.it>
2023-08-01 15:29:35 +0200

    Aggiungere i file di progetto.

---
+++ Helpers/WordHelper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using System.Windows.Media.Imaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+
+namespace DocMaker.Helpers
+{
+    public class WordHelper
+    {
+        private readonly string FILE_PATH;
+        public WordHelper(string _filePath)
+        {
+            FILE_PATH = _filePath;
+        }
+        public string CreateAndInsertAllPictures()
+        {
+            string wordFileName = System.IO.Path.Combine(FILE_PATH, "note.docx");
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(wordFileName, WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                string[] files = Directory.GetFiles(FILE_PATH, $"ScreenCapture*.png");
+                if (files != null && files.Length > 0)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        var img = new BitmapImage();
+                        using (var fs = new FileStream(files[i], FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            img.BeginInit();
+                            img.StreamSource = fs;
+                            img.EndInit();
+                        }
+                        int maxWidthCm = 15;
+                        var widthPx = img.PixelWidth;
+                        var heightPx = img.PixelHeight;
+                        var horzRezDpi = img.DpiX;
+                        var vertRezDpi = img.DpiY;
+                        const int emusPerInch = 914400;
+                        const int emusPerCm = 360000;
+                        var widthEmus = (long)(widthPx / horzRezDpi * emusPerInch);
+                        long heightEmus = (long)(heightPx / vertRezDpi * emusPerInch);
+                        long maxWidthEmus = (long)(maxWidthCm * emusPerCm);
+                        if (widthEmus > maxWidthEmus)
+                        {
+                            var ratio = (heightEmus * 1.0m) / widthEmus;
+                            widthEmus = maxWidthEmus;
+                            heightEmus = (long)(widthEmus * ratio);
+                        }
+                        ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+                        using (FileStream stream = new FileStream(files[i], FileMode.Open))
+                        {
+                            imagePart.FeedData(stream);
+                        }
+                        wordDocument.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(new Text("Step n. " + (i + 1)))));
+
+                        AddImageToBody(wordDocument, mainPart.GetIdOfPart(imagePart), widthEmus, heightEmus);
+                        File.Delete(files[i]);
+                    }
+                }
+            }
+            return wordFileName;
+        }
+
+        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId, long widthEmus, long heightEmus)
+        {
+            // Define the reference of the image.
+            var element =
+                 new Drawing(
+                     new DW.Inline(
+                         new DW.Extent() { Cx = widthEmus, Cy = heightEmus },
+                         new DW.EffectExtent()
+                         {
+                             LeftEdge = 0L,
+                             TopEdge = 0L,
+                             RightEdge = 0L,
+                             BottomEdge = 0L
+                         },
+                         new DW.DocProperties()
+                         {
+                             Id = (UInt32Value)1U,
+                             Name = "Picture 1"
+                         },
+                         new DW.NonVisualGraphicFrameDrawingProperties(
+                             new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                         new A.Graphic(
+                             new A.GraphicData(
+                                 new PIC.Picture(
+                                     new PIC.NonVisualPictureProperties(
+                                         new PIC.NonVisualDrawingProperties()
+                                         {
+                                             Id = (UInt32Value)0U,
+                                             Name = "New Bitmap Image.jpg"
+                                         },
+                                         new PIC.NonVisualPictureDrawingProperties()),
+                                     new PIC.BlipFill(
+                                         new A.Blip(
+                                             new A.BlipExtensionList(
+                                                 new A.BlipExtension()
+                                                 {
+                                                     Uri =
+                                                        "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                                                 })
+                                         )
+                                         {
+                                             Embed = relationshipId,
+                                             CompressionState =
+                                             A.BlipCompressionValues.Print
+                                         },
+                                         new A.Stretch(
+                                             new A.FillRectangle())),
+                                     new PIC.ShapeProperties(
+                                         new A.Transform2D(
+                                             new A.Offset() { X = 0L, Y = 0L },
+                                             new A.Extents() { Cx = widthEmus, Cy = heightEmus }),
+                                         new A.PresetGeometry(
+                                             new A.AdjustValueList()
+                                         )
+                                         {
+                                             Preset = A.ShapeTypeValues.Rectangle
+                                         }))
+                             )
+                             {
+                                 Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"
+                             })
+                     )
+                     {
+                         DistanceFromTop = (UInt32Value)0U,
+                         DistanceFromBottom = (UInt32Value)0U,
+                         DistanceFromLeft = (UInt32Value)0U,
+                         DistanceFromRight = (UInt32Value)0U,
+                         EditId = "50D07946"
+                     });
+
+            // Append the reference to body, the element should be in a Run.
+            wordDoc.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(element)));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using DocMaker.Helpers;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Vml.Office;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Gma.System.MouseKeyHook;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DocMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        public readonly string TEMP_PATH;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TEMP_PATH = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "DocMaker");
+            if (!Directory.Exists(TEMP_PATH))
+                Directory.CreateDirectory(TEMP_PATH);
+
+        }
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += M_GlobalHook_MouseDownExt;
+        }
+
+        private void M_GlobalHook_MouseDownExt(object? sender, MouseEventExtArgs e)
+        {
+            CaptureScreen(e.Location);
+        }
+
+        protected void CaptureScreen(System.Drawing.Point point)
+        {
+            double screenLeft = 0;
+            double screenTop = 0;
+            double screenWidth = 1920;
+            double screenHeight = 1080;
+
+            using Bitmap bmp = new Bitmap((int)screenWidth, (int)screenHeight);
+            using Graphics g = Graphics.FromImage(bmp);
+            string filename = "ScreenCapture-" + DateTime.Now.ToString("ddMMyyyy-hhmmss") + ".png";
+            Opacity = .0;
+            g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
+            int radius = 30;
+            System.Drawing.Pen p = new System.Drawing.Pen(System.Drawing.Color.Red);
+            g.DrawEllipse(p, point.X - radius, point.Y - radius,
+             radius + radius, radius + radius);
+            System.Drawing.Brush brush = new SolidBrush(System.Drawing.Color.FromArgb(50, 98, 0, 238));
+            g.FillEllipse(brush, point.X - radius, point.Y - radius,
+             radius + radius, radius + radius);
+            bmp.Save(System.IO.Path.Combine(TEMP_PATH, filename));
+            Opacity = 1;
+        }
+
+        private void BtTakeScreenshot_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            Subscribe();
+        }
+
+        private void BtStopScreenshot_Click(object sender, RoutedEventArgs e)
+        {
+            m_GlobalHook.Dispose();
+            string wordFileName = new WordHelper(TEMP_PATH).CreateAndInsertAllPictures();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Documento di Word (*.docx)|*.docx";
+            if (saveFileDialog.ShowDialog() == true)
+                File.Move(wordFileName, saveFileDialog.FileName);
+            OpenWithDefaultProgram(saveFileDialog.FileName);
+        }
+        public static void OpenWithDefaultProgram(string path)
+        {
+            using Process fileopener = new Process();
+
+            fileopener.StartInfo.FileName = "explorer";
+            fileopener.StartInfo.Arguments = "\"" + path + "\"";
+            fileopener.Start();
+        }
+    }
+}
